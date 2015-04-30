00a1c091d285f702a1cbe6e9009385ea09892fb0
Tim <thogg4@gmail.com>
2015-04-29 23:18:57 -0600

    rescue error with nil

--- app/helpers/spree/rotating_images_helper.rb
+++ app/helpers/spree/rotating_images_helper.rb
@@ -9,7 +9,7 @@ module Spree
       else
         images = nil
       end
-      images.inject('') { |string, image| string += "<li><img src='#{image.image.url}'></li>" }.html_safe rescue []
+      images.inject('') { |string, image| string += "<li><img src='#{image.image.url}'></li>" }.html_safe rescue nil
     end
 
   end
