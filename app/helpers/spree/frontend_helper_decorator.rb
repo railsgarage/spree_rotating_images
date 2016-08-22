410be88e471bec7d432108b21a77fa03d1616b1e
Tim Hogg <thogg4@gmail.com>
2016-08-22 17:46:42 -0600

    allow custom rotator placement

--- app/helpers/spree/frontend_helper_decorator.rb
+++ app/helpers/spree/frontend_helper_decorator.rb
@@ -2,9 +2,17 @@ Spree::FrontendHelper.module_eval do
 
   def rotating_images_for_params(params)
     if params[:controller] == 'spree/taxons' # we are on a taxon page
+
       images = Spree::RotatingImage.where(page: @taxon.name.downcase)
+
     elsif params[:controller] == 'spree/home' # we are on the home page
+
       images = Spree::RotatingImage.where(page: 'home')
+
+    elsif params.is_a?(String)
+
+      images = Spree::RotatingImage.where(page: params)
+
     else
       images = nil
     end
