24b2bdae94c1c1a0e1f540065dae6d186f43f996
Tim Hogg <thogg4@gmail.com>
2016-09-02 12:30:24 -0600

    set default width to fit

--- app/models/spree/rotating_image.rb
+++ app/models/spree/rotating_image.rb
@@ -1,6 +1,6 @@
 class Spree::RotatingImage < ActiveRecord::Base
 
-  has_attached_file :image, styles: { original: 'x300' }
+  has_attached_file :image, styles: { original: '1200' }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
